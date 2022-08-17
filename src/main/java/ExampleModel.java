97fb7ee8890325e659cf4adfab37f3e162560fa8
slawo <slawekpettke@gmail.com>
2022-08-17 19:37:15 +0200

    akcesory lombok przyklady

---
+++ src/main/java/ExampleModel.java
@@ -0,0 +1,42 @@
+public class ExampleModel {
+
+    private String hairColor;
+    private int height;
+    private int width;
+    private boolean isMale;
+
+    //normalnie dajemy settery i gettery
+
+
+    public String getHairColor() {
+        return hairColor;
+    }
+
+    public void setHairColor(String hairColor) {
+        this.hairColor = hairColor;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public boolean isMale() {
+        return isMale;
+    }
+
+    public void setMale(boolean male) {
+        isMale = male;
+    }
+}

6f4638b46734cfedf6c3bd5ca535365b662d31ad
slawo <slawekpettke@gmail.com>
2022-08-17 19:31:10 +0200

    ---

--- src/main/java/plt/learning/App.java
+++ src/main/java/plt/learning/App.java
@@ -1,16 +1,26 @@
 package plt.learning;
 
 import com.google.gson.Gson;
+import lombok.AllArgsConstructor;
 import lombok.Lombok;
 
 /**
  * Hello world!
  *
  */
+
+@AllArgsConstructor
 public class App 
 {
+    Boolean a;
+    Boolean b;
+    Boolean c;
     public static void main( String[] args )
     {
+        new App(true, true, true){
+        };
+
+
         System.out.println( "Hello World!" );
 
         new Gson();

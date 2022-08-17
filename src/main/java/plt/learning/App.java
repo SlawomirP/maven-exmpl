ea611572eb778aada9b8500a63effa044900df2f
slawo <slawekpettke@gmail.com>
2022-08-17 16:51:29 +0200

    Initial commit

---
+++ src/main/java/plt/learning/App.java
@@ -0,0 +1,13 @@
+package plt.learning;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
